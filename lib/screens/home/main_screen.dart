f6b9dd4fffa2b250c973f0e0475cb192b3666478
AndersonHenriqueRosa <anderson.rosa1@alunos.sc.senac.br>
2024-09-13 01:08:15 -0300

    Tela Transações

--- lib/screens/home/main_screen.dart
+++ lib/screens/home/main_screen.dart
@@ -1,108 +1,125 @@
 import 'package:flutter/material.dart';
-import 'package:juntaai/data/user_info.dart';
-import 'package:juntaai/widgets/income_expense_card.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:juntaai/data/user_info.dart';
+import 'package:juntaai/take_right.dart';
+import 'package:juntaai/widgets/custom_scaffold.dart';
+import 'package:juntaai/widgets/income_expense_card.dart';
 import 'package:juntaai/widgets/transaction_item_tile.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({Key? key}) : super(key: key);
+  
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(250.0), // Ajuste a altura da AppBar
-        child: AppBar(
-          backgroundColor: Colors.orange,
-          leading: null, // Remove o ícone de retorno
-          flexibleSpace: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 40.0, horizontal: 16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                ListTile(
-                  title: Text("Olá! ${userdata.name}!"),
-                  leading: ClipRRect(
-                    borderRadius: const BorderRadius.all(Radius.circular(16.0)),
-                    child: Image.asset("assets/imgs/anderson.jpeg"),
+    // Obtém os últimos 10 itens da lista e inverte a lista para mostrar o mais recente primeiro
+    final recentTransactions = userdata.transaction
+        .takeRight(10) // Obtém os últimos 10 itens da lista
+        .toList()
+        .reversed // Inverte a lista para mostrar o mais recente primeiro
+        .toList();
+
+    return CustomScaffold(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            color: Colors.orange, // Cor do fundo da seção superior
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ListTile(
+                    title: Text(
+                      "Olá, ${userdata.name}!",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    leading: ClipRRect(
+                      borderRadius: const BorderRadius.all(Radius.circular(20.0)),
+                      child: Image.asset("assets/imgs/anderson.jpeg"),
+                    ),
+                    trailing: const Icon(CupertinoIcons.bell_solid, color: Colors.white),
                   ),
-                  trailing: const Icon(CupertinoIcons.bell, color: Colors.white),
-                ),
-                const SizedBox(height: 16.0),
-                Center(
-                  child: Column(
+                  const SizedBox(height: 16.0),
+                  Center(
+                    child: Column(
+                      children: [
+                        Text(
+                          "Saldo Atual",
+                          style: Theme.of(context).textTheme.bodyLarge?.copyWith(color: Colors.white),
+                        ),
+                        const SizedBox(height: 8.0),
+                        Text(
+                          userdata.totalBalance,
+                          style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                              fontSize: 18.0, fontWeight: FontWeight.w800, color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 16.0),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      Text(
-                        "Saldo Atual",
-                        style: Theme.of(context)
-                            .textTheme
-                            .bodyLarge
-                            ?.copyWith(color: Colors.white),
+                      Expanded(
+                        child: IncomeExpenseCard(
+                          expenseData: ExpenseData(
+                            Icons.arrow_upward_rounded,
+                            "Receita",
+                            userdata.inFlow,
+                          ),
+                        ),
                       ),
-                      const SizedBox(height: 8.0),
-                      Text(
-                        userdata.totalBalance,
-                        style: Theme.of(context).textTheme.titleLarge?.copyWith(
-                            fontSize: 18.0, fontWeight: FontWeight.w800),
+                      const SizedBox(width: 16.0),
+                      Expanded(
+                        child: IncomeExpenseCard(
+                          expenseData: ExpenseData(
+                            Icons.arrow_downward_rounded,
+                            "Despesa",
+                            userdata.outFlow,
+                          ),
+                        ),
                       ),
                     ],
                   ),
-                ),
-                const SizedBox(height: 16.0),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Expanded(
-                      child: IncomeExpenseCard(
-                        expenseData: ExpenseData(Icons.arrow_upward_rounded, "Receita", userdata.inFlow),
-                      ),
-                    ),
-                    const SizedBox(width: 16.0),
-                    Expanded(
-                      child: IncomeExpenseCard(
-                        expenseData: ExpenseData(Icons.arrow_downward_rounded, "Despesa", userdata.outFlow),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
+                ],
+              ),
             ),
           ),
-        ),
-      ),
-      body: Container(
-        color: Colors.orange, // Cor de fundo da tela
-        child: Column(
-          children: [
-            Container(
+          // Seção de transações recentes que ocupará o restante do espaço disponível
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.fromLTRB(25.0, 20.0, 25.0, 20.0),
               decoration: const BoxDecoration(
-                color: Colors.white, // Cor de fundo do conteúdo
+                color: Colors.white,
                 borderRadius: BorderRadius.only(
                   topLeft: Radius.circular(40.0),
                   topRight: Radius.circular(40.0),
                 ),
               ),
               child: SingleChildScrollView(
-                padding: const EdgeInsets.all(18.0),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    const SizedBox(height: 32.0),
+                    const SizedBox(height: 20.0),
                     Text(
                       "Transações Recentes",
                       style: Theme.of(context).textTheme.titleLarge?.copyWith(fontWeight: FontWeight.w700),
                     ),
                     const SizedBox(height: 16.0),
-                    ...userdata.transaction
+                    ...recentTransactions
                         .map((transaction) => TransactionItemTile(transaction: transaction))
                         .toList(),
                   ],
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
 }
+
+
--- lib/screens/transactions/transactions_screen.dart
+++ lib/screens/transactions/transactions_screen.dart
@@ -1,6 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:juntaai/data/user_info.dart';
 import 'package:juntaai/screens/transactions/expense_screen.dart';
 import 'package:juntaai/screens/transactions/income_screen.dart';
+import 'package:juntaai/widgets/custom_scaffold.dart';
+import 'package:juntaai/widgets/income_expense_card.dart';
+import 'package:juntaai/widgets/transaction_item_tile.dart';
 
 class TransactionsScreen extends StatefulWidget {
   const TransactionsScreen({super.key});
@@ -12,84 +17,118 @@ class TransactionsScreen extends StatefulWidget {
 class _TransactionsScreenState extends State<TransactionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const IncomeScreen(),
+    final allTransactions = userdata.transaction
+        .toList()
+        .reversed // Inverte a lista para mostrar o mais recente primeiro
+        .toList();
+        
+    return CustomScaffold(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            color: Colors.orange, // Cor do fundo da seção superior
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      IconButton(
+                        icon: const Icon(CupertinoIcons.back, color: Colors.white),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                      ),
+                      const SizedBox(width: 8.0), // Espaço entre o ícone e o texto
+                      Text(
+                        "Transações",
+                        style: Theme.of(context).textTheme.headlineLarge?.copyWith(
+                              color: Colors.white,
+                              fontSize: 24.0, // Ajuste o tamanho da fonte conforme necessário
+                            ),
+                      ),
+                    ],
                   ),
-                );
-              },
-              child: Container(
-                width: 150, // Largura do botão
-                height: 50, // Altura padrão do botão
-                decoration: BoxDecoration(
-                  color: Colors.green, // Cor de fundo do botão
-                  borderRadius: BorderRadius.circular(30), // Bordas arredondadas
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.2),
-                      offset: const Offset(0, 4),
-                      blurRadius: 6,
+                  const SizedBox(height: 8.0), // Espaço reduzido entre o texto e o saldo
+                  Center(
+                    child: Column(
+                      children: [
+                        Text(
+                          "Saldo Atual",
+                          style: Theme.of(context).textTheme.bodyLarge?.copyWith(color: Colors.white),
+                        ),
+                        const SizedBox(height: 8.0),
+                        Text(
+                          userdata.totalBalance,
+                          style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                              fontSize: 18.0, fontWeight: FontWeight.w800, color: Colors.white),
+                        ),
+                      ],
                     ),
-                  ],
+                  ),
+                  const SizedBox(height: 16.0),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Expanded(
+                        child: IncomeExpenseCard(
+                          expenseData: ExpenseData(
+                            Icons.arrow_upward_rounded,
+                            "Receita",
+                            userdata.inFlow,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 16.0),
+                      Expanded(
+                        child: IncomeExpenseCard(
+                          expenseData: ExpenseData(
+                            Icons.arrow_downward_rounded,
+                            "Despesa",
+                            userdata.outFlow,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          // Seção de transações recentes que ocupará o restante do espaço disponível
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.fromLTRB(25.0, 20.0, 25.0, 20.0),
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(40.0),
+                  topRight: Radius.circular(40.0),
                 ),
-                child: const Center(
-                  child: Text(
-                    'Receita',
-                    style: TextStyle(
-                      fontSize: 18.0,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white, // Cor do texto
+              ),
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 20.0),
+                    Text(
+                      "Transações",
+                      style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                            fontWeight: FontWeight.w700,
+                          ),
                     ),
-                  ),
+                    const SizedBox(height: 16.0),
+                    ...allTransactions
+                        .map((transaction) => TransactionItemTile(transaction: transaction))
+                        .toList(),
+                  ],
                 ),
               ),
             ),
-            const SizedBox(width: 10), // Espaçamento entre os botões
-            GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const ExpenseScreen(),
-                  ),
-                );
-              },
-              child: Container(
-                width: 150, // Largura do botão
-                height: 50, // Altura padrão do botão
-                decoration: BoxDecoration(
-                  color: Colors.red, // Cor de fundo do botão
-                  borderRadius: BorderRadius.circular(30), // Bordas arredondadas
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.2),
-                      offset: const Offset(0, 4),
-                      blurRadius: 6,
-                    ),
-                  ],
-                ),
-                child: const Center(
-                  child: Text(
-                    'Despesa',
-                    style: TextStyle(
-                      fontSize: 18.0,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white, // Cor do texto
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
